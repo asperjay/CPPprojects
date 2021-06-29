b251c6555f782f371abefba9a4b8c270d4fe08c0
asperjay <54516471+asperjay@users.noreply.github.com>
2021-06-29 15:04:59 -0700

    a blind shield
    who runs to the nose

---
+++ schrodinger.cpp
@@ -0,0 +1,19 @@
+// Student ID: 20445198
+
+#include <iostream>
+
+using namespace std;
+
+void draw_cat() {
+    std::cout << " -------" << endl;
+    std::cout << "| /\\_/\\ |" << endl;
+    std::cout << "|( o o )|" << endl;
+    std::cout << "| > ^ < |" << endl;
+    std::cout << " -------" << endl;
+}
+
+int main(int argc, const char * argv[])
+{
+    draw_cat();
+    return 0;
+}
